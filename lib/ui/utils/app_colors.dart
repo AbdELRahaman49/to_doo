b813a5286fe8217c0ddf825fb40c65b6104f5f7f
AbdELRahaman49 <abdomohamed49000@gmail.com>
2023-12-02 09:39:48 +0200

    Initial commit

---
+++ lib/ui/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+abstract class Appcolors {
+  static const Color primiary = Color (0xff3873a9);
+  static const Color accent = Color (0xffddeada);
+  static const Color white = Colors.white;
+  static const Color transparent = Colors.transparent;
+  static const Color primiaryDark = primiary;
+  static const Color accentDark = Color (0xff15152d);
+  static const Color lightBlack = Color (0xff363636);
+  static const Color black = Colors.black;
+  static const Color grey = Color (0xffC8C9CB);
+}
---
+++ lib/ui/utils/app_theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:to_do/ui/utils/app_colors.dart';
+abstract class AppTheme {
+  static const TextStyle appBarTextStyle = TextStyle(
+    fontWeight: FontWeight.bold,fontSize: 22, color: Appcolors.white);
+  static const TextStyle taskTitleTextStyle = TextStyle(
+      fontWeight: FontWeight.bold,fontSize: 22, color: Appcolors.primiary);
+  static const TextStyle taskDescriptionTextStyle = TextStyle(
+      fontWeight: FontWeight.normal,fontSize: 14, color: Appcolors.lightBlack);
+  static const TextStyle bottomSheetTitleTextStyle = TextStyle(
+      fontWeight: FontWeight.bold,fontSize: 20, color: Appcolors.black);
+  static ThemeData lightTheme =  ThemeData(
+    primaryColor: Appcolors.primiary,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Appcolors.primiary,
+      elevation: 0,
+      titleTextStyle: appBarTextStyle,
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      selectedIconTheme: IconThemeData(size: 32),
+      unselectedIconTheme: IconThemeData(size: 32),
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      selectedItemColor: Appcolors.primiary,
+      unselectedItemColor: Appcolors.grey,
+    ),
+    scaffoldBackgroundColor: Appcolors.accent,
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      shape: StadiumBorder(
+        side:  BorderSide(color: Appcolors.white,width: 4)
+      ),
+    )
+  );
+  static ThemeData darkTheme = ThemeData();
+}
